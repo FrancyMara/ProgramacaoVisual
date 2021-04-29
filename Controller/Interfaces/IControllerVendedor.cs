3b98fd094c6c553710058fcad6b1a5c85d8bc877
FrancyMara <franchy.mara@gmail.com>
2021-04-29 17:44:30 -0300

    update

---
+++ Controller/Interfaces/IControllerVendedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Entidades;
+
+namespace Controller.Interfaces
+{
+    interface IControllerVendedor
+    {
+        bool Salvar(Vendedor vendedor , bool Editando);
+        bool Remover(Vendedor vendedor);
+        List<Vendedor> GetVendedor();
+        Vendedor GetVendedor(string nome);
+
+    }
+}
--- Modelo/Entidades/Vendedor.cs
+++ Modelo/Entidades/Vendedor.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Modelo.Entidades
 {
-    public class Vendedor: Pessoa
+    public class Vendedor : Pessoa
     {
+        public string ID { get; set; }
+
+        public string CPF { get; set; }
+
+        public  Vendedor() {}
+
+
+    public override bool Equals(object obj)
+    {
+            if (obj == null || obj?.GetType() != typeof(Vendedor))
+                return false;
+
+            var V = (obj as Vendedor);
+
+            return ID.Equals(V.ID);
+
     }
+        public override string ToString()
+        {
+            return $"ID: {ID}, {Nome}, CPF: {CPF}";
+        }
+
+
+    }
+
+
+
+    
+
+
 }
